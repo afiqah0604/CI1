34487af8cf13528d0d1a866915f9ef1a43c7970a
afiqah0604 <dalilah@iris.com.my>
2024-09-09 14:52:44 +0800

    Update python-job-dsl.groovy

--- python-job-dsl.groovy
+++ python-job-dsl.groovy
@@ -3,7 +3,7 @@ job('Python-Build-Job') {
 
     // Source Code Management
     scm {
-        git('https://github.com/your-repo/python-project.git')
+        git('https://github.com/afiqah0604/python-project.git')
     }
 
     // Build Triggers
